0726df4619b953d23a677241f00466d735cbb3ad
Anthony <fioritoanthony@outlook.com>
2017-01-29 21:04:21 -0500

    added react router

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
 // the only scss require, other files can be imported inside main.scss
 // check main.scss for more info.
@@ -10,11 +11,11 @@ import NoteShare from './NoteShare';
 /**
  * Bootstraps the app by linking it to the div with id=app in index.html
  */
-export default class App extends React.Component {
-    render() {
-        return <NoteShare />;
-    }
-}
 
 // bootstrap
-ReactDOM.render(<App />, document.getElementById('app'));
+render((
+    <Router history={browserHistory}>
+        <Route path="/" component={NoteShare} />
+    </Router>
+
+), document.getElementById('app'));
