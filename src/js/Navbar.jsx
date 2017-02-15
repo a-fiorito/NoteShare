f4b67eef10f6f771bd2fb02fda730a47b3c5cd7e
Vartan Benohanian <vartanbeno@gmail.com>
2017-02-15 14:36:14 -0500

    styled up the header and navbar

--- src/js/Navbar.jsx
+++ src/js/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router'
+import {Link} from 'react-router';
 
 class Navbar extends Component {
 	constructor(props) {
@@ -7,18 +7,21 @@ class Navbar extends Component {
 
 	}
 
-
 	render() {
 		let user = this.props.user;
 		return (
 			<div className="nav-bar">
-				<div className="brand"><p>NoteShare</p></div>
-				<ul>
-					<li><Link to="/">Home</Link></li>
-					{user ? <li><Link to="/dashboard">Dashboard</Link></li> : null}
-					<li>Profile</li>
-					{user ? <li onClick={this.props.signOut.bind(this)}>Log out</li> : <li><Link to="/login">Signup/Signin</Link></li>}
-				</ul>
+				<div className="brand">NoteShare</div>
+				<div className="menu">
+					<ul>
+						<li><Link to="/">Home</Link></li>
+						{user ? <li><Link to="/dashboard">Dashboard</Link></li> : null}
+						{user ? <li><Link>Profile</Link></li> : null}
+						{/*<li><Link>FAQ</Link></li>*/}
+						{/*<li><Link>Contact</Link></li>*/}
+						{user ? <li onClick={this.props.signOut.bind(this)}><Link to="/login">Log Out</Link></li> : <li><Link to="/login">Sign In / Sign Up</Link></li>}
+					</ul>
+				</div>
 			</div>
 		);
 	}
