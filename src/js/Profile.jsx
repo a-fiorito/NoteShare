0b4a4d5d9c389cf396cccefefca09ab75650ce61
dstroppolo <stroppolo.daniel@gmail.com>
2017-02-14 23:49:21 -0500

    Added a profile page and button component
    I added a profile page which is pretty empty at the momment.
    Created button components for adding documents and classes

--- src/js/Profile.jsx
+++ src/js/Profile.jsx
@@ -7,7 +7,7 @@ import Button from './Button';
 Profile component which should appear when a user clicks on the 'view profile' button
 */
 
-const user = {id: '2', fname: 'Daniel', lname: 'Stroppolo', username: 'dstroppolo'};
+const user = {id: '2', fname: 'Daniel', lname: 'Stroppolo', username: 'dstroppolo', type: 'student'};
 
 
 class Name extends Component {
@@ -20,7 +20,22 @@ class Name extends Component {
     return(
       <div>
         <h1>{this.props.fname} {this.props.lname}</h1>
-        <h2>{this.props.username}</h2>
+        <h2>{this.props.username} | {this.props.type}</h2>
+      </div>
+    );
+  }
+}
+
+class Explanation extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='explanation'>
+        <p>Welcome to your profile! From here, you can view notes you have uploaded, upload new notes and create or join classes.</p>
       </div>
     );
   }
@@ -31,18 +46,8 @@ export default class Profile extends Component {
     super(props);
   }
 
-  addNote = () => {
-    alert('Adding a note');
-  }
-
-  addClass = () => {
-    alert('Adding a class');
-  }
-
-  //not sure if this is how its done. We pass a function to call as a param
-  //set up some examples.
-  displayButton(label, action){
-    return <Button label={label} function={action} />
+  displayButton(label, func){
+    return <Button label={label} func={func}/>
   }
 
   //user object as a parameter
@@ -50,16 +55,27 @@ export default class Profile extends Component {
     return <Name {...user} />
   }
 
+  displayExplanation(){
+    return <Explanation />
+  }
+
+  /*My idea for this section is to eventually add a DocumentArea which displays
+  the notes that you have uploaded, so you can delete them? or update them? For
+  example, for the thumbnail component have a state called edit where these
+  options appear.
+  feel free to remove the DocumentArea, I just didn't know what else to put on
+  the page.*/
   render() {
     return(
       <div className='profile-area'>
         <div className='name-button-wrapper'>
-          {this.displayName(user)}{/*pass the student object we will eventually get from the db*/}
+          {this.displayName(user)}{/*pass the user object we will eventually get from the db*/}
           <div className='button-wrapper'>
-            {this.displayButton('Add a note', this.addNote)}
-            {this.displayButton('Add a class', this.addClass)}
+            {this.displayButton('Add a note', 'doc')}
+            {this.displayButton('Add or join a class', 'class')}
           </div>
         </div>
+        {this.displayExplanation()}
         <DocumentArea />
       </div>
 
