22fae0eb1ccc16b23425608e92025196b6630375
Anthony <fioritoanthony@outlook.com>
2017-02-16 10:43:00 -0500

    Merged all team members work Fixed navbar to take up less space, added profile link Styled login/signup page Fixed sidebar, added the ability to select which course to view notes for Styled the document area component Styled the profile component Created the icon and favicon for the app Imported two fonts Roboto and Sansita Included a couple of icons to improve UX

--- src/js/Navbar.jsx
+++ src/js/Navbar.jsx
@@ -9,23 +9,28 @@ class Navbar extends Component {
 
 	render() {
 		let user = this.props.user;
+		let location = this.context.router.location.pathname;
 		return (
 			<div className="nav-bar">
-				<div className="brand">NoteShare</div>
+				<div className="brand"><img src="/assets/images/book.svg" />NOTESHARE</div>
 				<div className="menu">
 					<ul>
-						<li><Link to="/">Home</Link></li>
-						{user ? <li><Link to="/dashboard">Dashboard</Link></li> : null}
-						{user ? <li><Link>Profile</Link></li> : null}
+						<li><Link to="/" onlyActiveOnIndex activeClassName="active">home</Link></li>
+						{user ? <li><Link to="/dashboard" activeClassName="active">dashboard</Link></li> : null}
+						{user ? <li><Link to="/profile" activeClassName="active">profile</Link></li> : null}
 						{/*<li><Link>FAQ</Link></li>*/}
 						{/*<li><Link>Contact</Link></li>*/}
-						{user ? <li onClick={this.props.signOut.bind(this)}><Link to="/login">Log Out</Link></li> : <li><Link to="/login">Sign In / Sign Up</Link></li>}
+						{user ? <li onClick={this.props.signOut.bind(this)}><Link to="/login">log out</Link></li> : <li><Link to="/login" className={location == "/signup" ? "active" : null} activeClassName="active">sign in / sign up</Link></li>}
 					</ul>
 				</div>
 			</div>
 		);
 	}
 
+}
+
+Navbar.contextTypes = {
+    router: React.PropTypes.object.isRequired
 }
 
 export default Navbar;
