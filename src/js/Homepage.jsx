52fa335319817ce8981617c53ee76593a316d0b2
Adam-shafey <abdoshafey1@gmail.com>
2017-02-16 18:04:59 -0500

    Feature set of homepage
    Added the feature set of the homepage

--- src/js/Homepage.jsx
+++ src/js/Homepage.jsx
@@ -9,6 +9,44 @@ export default class Homepage extends Component {
         return (
             <div className="home-page">
                 <Header />
+                  <div id="big-container">
+                    <div id="container3">
+                    <div id="container2">
+                        <div id="container1">
+                            <div id="col1"><center><p><img src="./assets/HomePageIcons/1.png" width="50" height="50"></img></p>
+                            <h3>DOWNLOAD NOTES</h3>
+                            <p>Download notes from courses that you follow</p>
+                            </center></div>
+                            <div id="col2"><center><p><img src="./assets/HomePageIcons/2.png" width="50" height="50"></img></p>
+                            <h3>UPLOAD NOTES</h3>
+                            <p>Upload notes to courses that you follow so everyone can see them</p>
+                            </center></div>
+                            <div id="col3"><center><p><img src="./assets/HomePageIcons/3.png" width="50" height="50"></img></p>
+                            <h3>POST DISCUSSION</h3>
+                            <p>Post comments on notes and get your reviewed</p>
+                            </center></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="container3">
+                    <div id="container2">
+                        <div id="container1">
+                            <div id="col1"><center><p><img src="./assets/HomePageIcons/4.png" width="50" height="50"></img></p>
+                            <h3>GET NOTIFIED</h3>
+                            <p>Get notified when a course that you follow gets a new note or comment</p>
+                            </center></div>
+                            <div id="col2"><center><p><img src="./assets/HomePageIcons/5.png" width="50" height="50"></img></p>
+                            <h3>LANGUAGE MODERATED</h3>
+                            <p>Posts are automatically moderated to make sure no foul language is used</p>
+                            </center></div>
+                            <div id="col3"><center><p><img src="./assets/HomePageIcons/6.png" width="50" height="50"></img></p>
+                            <h3>FOREVER FREE</h3>
+                            <p>Made by students and will always remain free</p>
+                            </center></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -25,7 +63,7 @@ class Header extends Component {
                             <div className="btn">Get Started</div>
                             <div className="btn">Learn More</div>
                         </div>
-                    </div> 
+                    </div>
             </header>
         );
     }
