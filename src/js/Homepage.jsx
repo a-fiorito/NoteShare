15621d97de2a95a021c3c65ca82e9bb76a74f944
Anthony <fioritoanthony@outlook.com>
2017-02-20 14:56:54 -0500

    Refactored adam homepage code - cleaned up css to nest selectors - cleaned up html code by splitting reusuable code into components

--- src/js/Homepage.jsx
+++ src/js/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 /*
     Splash page to introduce website
@@ -7,13 +7,11 @@ export default class Homepage extends Component {
 
     render() {
         return (
-          <div>
             <div className="home-page">
                 <Header />
                 <Features />
+                <Footer />
             </div>
-            <Footer />
-          </div>
         );
     }
 }
@@ -21,8 +19,8 @@ export default class Homepage extends Component {
 class Header extends Component {
     render() {
         return (
-          <div className="home-page">
-            <header className="homepage-top">
+            <div className="home-page">
+                <header className="homepage-top">
                     <div className="top-content">
                         <h1>Note Share</h1>
                         <p>Share notes with classmates, get your assignments graded</p>
@@ -31,67 +29,68 @@ class Header extends Component {
                             <div className="btn">Learn More</div>
                         </div>
                     </div>
-            </header>
+                </header>
             </div>
         );
     }
 }
 
 class Features extends Component {
-  render (){
-    return(
-        <div className="big-container">
-          <div id="container3">
-          <div id="container2">
-              <div id="container1">
-                  <div id="col1"><center><p><img src="./assets/HomePageIcons/1.png" width="50" height="50"></img></p>
-                  <h3>DOWNLOAD NOTES</h3>
-                  <p>Download notes from courses that you follow</p>
-                  </center></div>
-                  <div id="col2"><center><p><img src="./assets/HomePageIcons/2.png" width="50" height="50"></img></p>
-                  <h3>UPLOAD NOTES</h3>
-                  <p>Upload notes to courses that you follow so everyone can see them</p>
-                  </center></div>
-                  <div id="col3"><center><p><img src="./assets/HomePageIcons/3.png" width="50" height="50"></img></p>
-                  <h3>POST DISCUSSION</h3>
-                  <p>Post comments on notes and get your reviewed</p>
-                  </center></div>
-                  </div>
-              </div>
-          </div>
-          <div id="container3">
-          <div id="container2">
-              <div id="container1">
-                  <div id="col1"><center><p><img src="./assets/HomePageIcons/4.png" width="50" height="50"></img></p>
-                  <h3>GET NOTIFIED</h3>
-                  <p>Get notified when a course that you follow gets a new note or comment</p>
-                  </center></div>
-                  <div id="col2"><center><p><img src="./assets/HomePageIcons/5.png" width="50" height="50"></img></p>
-                  <h3>LANGUAGE MODERATED</h3>
-                  <p>Posts are automatically moderated to make sure no foul language is used</p>
-                  </center></div>
-                  <div id="col3"><center><p><img src="./assets/HomePageIcons/6.png" width="50" height="50"></img></p>
-                  <h3>FOREVER FREE</h3>
-                  <p>Made by students and will always remain free</p>
-                  </center></div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="features-container">
+                <Feature
+                    imgSrc="./assets/HomePageIcons/1.png"
+                    header="DOWNLOAD NOTES"
+                    body="Download notes from courses that you follow."
+                />
+                <Feature
+                    imgSrc="./assets/HomePageIcons/2.png"
+                    header="UPLOAD NOTES"
+                    body="Upload notes to courses that you follow so everyone can see them."
+                />
+                <Feature
+                    imgSrc="./assets/HomePageIcons/3.png"
+                    header="POST DISCUSSION"
+                    body="Post comments on notes and get your reviewed."
+                />
+                <Feature
+                    imgSrc="./assets/HomePageIcons/4.png"
+                    header="GET NOTIFIED"
+                    body="Get notified when a course that you follow gets a new note or comment."
+                />
+                <Feature
+                    imgSrc="./assets/HomePageIcons/5.png"
+                    header="LANGUAGE MODERATED"
+                    body="Posts are automatically moderated to make sure no foul language is used."
+                />
+                <Feature
+                    imgSrc="./assets/HomePageIcons/6.png"
+                    header="FOREVER FREE"
+                    body="Made by students and will always remain free."
+                />
+            </div>
+        )
+    }
+}
+
+const Feature = ({imgSrc, header, body}) => {
+    return (
+        <div className="feature">
+            <img src={imgSrc} width="50" height="50"></img>
+            <h3>{header}</h3>
+            <p>{body}</p>
+        </div>
+    );
 }
 
 class Footer extends Component {
     render() {
         return (
-          <div className="home-page">
-              <div className="footer">
-                  <div className="brand">NOTESHARE</div>
-                  <div className="copyright"> Made with love in concordias labs </div>
-              </div>
-          </div>
-
+            <div className="footer">
+                <div className="brand">NOTESHARE</div>
+                <div className="copyright"> Made with love in concordias labs </div>
+            </div>
         );
     }
 }
