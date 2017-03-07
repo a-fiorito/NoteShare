0f9990f9a6b5a51d7ebaafffbcc9b81dc029b34b
dstroppolo <stroppolo.daniel@gmail.com>
2017-03-06 23:43:54 -0500

    Added upload modal and connect view to backend
    Readded my part:
    Added upload button to dashboard
    Now able to upload documents.

--- src/js/Dashboard.jsx
+++ src/js/Dashboard.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import Button from './Button';
 import DocumentArea from './DocumentArea';
 import Sidebar from './Sidebar';
+import Modal from './Modal';
 
 /*
     Main app view for viewing/uploading pdfs
@@ -23,10 +24,13 @@ export default class Dashboard extends Component {
 
     render() {
         return (
+          <div>
+            <Modal />
             <div className="dashboard">
-                <Sidebar />
-                <DocumentArea />
+              <Sidebar />
+              <DocumentArea />
             </div>
+          </div>
         );
     }
 }
