9c05642f5b0f188600ea387ab427022ed39a4dde
dstroppolo <stroppolo.daniel@gmail.com>
2017-04-04 23:05:28 -0400

    unit tests for the upload modal
    Testing to see if the upload modal works as it should.
    Test that the component mounts
    that the button will call the upload function
    that the files to be uploaded are set properly
    that the dashboard state will change once uploaded

--- __tests__/upload.test.js
+++ __tests__/upload.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import DocumentArea from '../src/js/DocumentArea/DocumentArea'
 import UploadModal from '../src/js/Dashboard/UploadModal';
-import Dashboard from '../src/js/Dashboard/Dashboard'
+import Dashboard from '../src/js/Dashboard/Dashboard';
+import Button from '../src/js/Abstract/Button';
 
 describe('Uploading documents', () => {
     
@@ -29,10 +30,9 @@ describe('Uploading documents', () => {
             }
         };
     }
-
-
     // mock the localStorage
     window.localStorage = storageMock();
+
     const userProp = {    
                     id: 1,
                     username: 'Username',
@@ -50,43 +50,71 @@ describe('Uploading documents', () => {
 
     it('calls componentDidMount', () => {
     
-    expect(
-        shallow(
-            <Dashboard />
-        ).length
-    ).toEqual(1);
+    expect( shallow(<Dashboard />).length).toEqual(1);
   });
 
 
 
+  it('will call the upload function', () => {
+
+    //mount the modal and give it a state and props so the button will not be disabled
+    const uploadModal = mount(<UploadModal />);
+    let state = { 
+                files: [new File(['data'], 'filename')], 
+                filename: 'filename',
+                success: false,
+                isDisabled: false,
+        };
+    
+    let props = {
+                selectedCourse: {id: 1, name: 'SOEN', number: '341'},
+                user: {id: 1, username: 'user'},
+        };
+
+    uploadModal.setState(state);
+    uploadModal.setProps(props);
+
+    expect(uploadModal.contains(<Button />));
+    
+    const onUpload = jest.fn();
+    const button = mount(<Button label={'label'} func={onUpload} />);
+    button.simulate('click');
+        expect(onUpload).toBeCalled();
+
+  })
+
+  it('wil add a new document to the state in preparation of uploading', () => {
+
+    const uploadModal = shallow(<UploadModal />);
+    let acceptedFiles = [];
+
+    //create the files we are passing
+    let acceptedFile = new File(["content"], 'filename');
+    acceptedFiles.push(acceptedFile);
+    let otherFile = null;
+
+        expect(uploadModal.state().files.length).toEqual(0);
+    
+    //file is ready to be uploaded
+    uploadModal.instance().onDrop(acceptedFiles, otherFile);
+        expect(uploadModal.state().files.length).toEqual(1);
+        expect(uploadModal.state().filename).toEqual('filename');
+  })
+
   it('will add a new document to the state once uploaded', () => {
 
-          //document has already been uploaded
+    //document has already been uploaded
     const dashboard = shallow(<Dashboard />);
         expect(dashboard.state().documents.length).toEqual(0);
 
     dashboard.setProps({user: userProp});
 
-
-
-      dashboard.instance().updateDocuments(doc);          
+    dashboard.instance().updateDocuments(doc);          
         expect(dashboard.state('documents').length).toEqual(1); 
         expect(dashboard.state('documents')[0].name).toEqual('New Document');
         expect(dashboard.state('documents')[0].user.username).toEqual('Username');
   });
 
-  it('will add a new thumbnail to the page once uploaded', () => {
-
-    const dashboard = mount(<Dashboard />);
-        expect(dashboard.contains(<DocumentArea />)).toEqual(false);
-
-        dashboard.instance().updateDocuments(doc);
-        dashboard.instance().updateDocuments(doc);
-
-        expect(dashboard.contains(<DocumentArea />)).toEqual(true);
-
-
-  })
 
 
 
